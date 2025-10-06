1ef39a920b532c2b15c2f58a3fabd7f1ce91673e
Blazoi <jackjackdieu@gmail.com>
2025-10-06 18:09:03 -0400

    Fini le script

--- cours.sql
+++ cours.sql
@@ -1,29 +1,29 @@
+ALTER SESSION SET CONTAINER = CDB$ROOT;
+-- DBA principal et son collègue
+CREATE USER C##sys1 IDENTIFIED BY oracle;
+CREATE USER C##sys2 IDENTIFIED BY oracle;
+-- Rôle de SYSDBA parce qu'ils sont les administrateurs de la base de données.
+GRANT SYSDBA TO C##sys1;
+GRANT SYSDBA TO C##sys2;
+
 -- Création de la BD
 CREATE PLUGGABLE DATABASE TP1PDB
-ADMIN USER admin IDENTIFIED BY oracle
+ADMIN USER JAD_gestionnaire IDENTIFIED BY oracle
 FILE_NAME_CONVERT = ('/opt/oracle/oradata/FREE/pdbseed/',
-'/opt/oracle/oradata/FREE/tp1pdb/')
+'/opt/oracle/oradata/FREE/tp1pdb/');
+
+ALTER SESSION SET CONTAINER = TP1PDB;
 
 -- Utilisateurs
--- DBA principal et son collègue
-CREATE USER sys1 IDENTIFIED BY oracle;
-CREATE USER sys2 IDENTIFIED BY oracle;
--- Rôle de SYSDBA parce qu'ils sont les administrateurs de la base de données.
-GRANT SYSDBA TO sys1;
-GRANT SYSDBA TO sys2;
-
--- Gestionnaire
-CREATE USER JAD_gestionnaire IDENTIFIED BY oracle;
-
 -- Registrariat:
-CREATE USER JAD_registrariat IDENTIFIED BY oracle
-
+CREATE USER JAD_registrariat IDENTIFIED BY oracle;
 -- API
-CREATE USER JAD_api IDENTIFIED BY oracle
-
+CREATE USER JAD_api IDENTIFIED BY oracle;
 -- Enseignant
-CREATE USER JAD_enseignant IDENTIFIED BY oracle
+CREATE USER JAD_enseignant IDENTIFIED BY oracle;
 
+
+-- Création des tables
 CREATE TABLE JAD_registrariat.etudiants (
     da_etudiant NUMBER PRIMARY KEY,
     nom VARCHAR2(50),
@@ -63,19 +63,19 @@ GRANT DBA TO r_gestion;
 CREATE ROLE r_registrariat;
 GRANT CREATE SESSION TO r_registrariat;
 GRANT RESOURCE TO r_registrariat;
-GRANT ALL ON etudiants to r_registrariat;
-GRANT ALL ON cours to r_registrariat;
+GRANT ALL ON JAD_registrariat.etudiants to r_registrariat;
+GRANT ALL ON JAD_registrariat.cours to r_registrariat;
 
 CREATE ROLE r_api;
 GRANT CREATE SESSION TO r_api;
 GRANT RESOURCE TO r_api;
-GRANT ALL ON groupes to r_api;
-GRANT ALL ON semestres to r_api;
+GRANT ALL ON JAD_api.groupes to r_api;
+GRANT ALL ON JAD_api.semestres to r_api;
 
 CREATE ROLE r_enseignant;
 GRANT CREATE SESSION TO r_enseignant;
 GRANT RESOURCE TO r_enseignant;
-GRANT ALL ON evaluations TO r_enseignant;
+GRANT ALL ON JAD_enseignant.evaluations TO r_enseignant;
 
 -- Profils
 -- Limitations sont beaucoup moins grandes ce qui rend ce profil moins restrictif
@@ -84,7 +84,7 @@ IDLE_TIME 30
 FAILED_LOGIN_ATTEMPTS 5
 PASSWORD_LOCK_TIME 5/1440
 PASSWORD_LIFE_TIME 90
-PASSWORD_GRACE_TIME 7
+PASSWORD_GRACE_TIME 7;
 
 -- Limitations sont beaucoup plus strictes et standard avec des conséquences plus grandes
 -- de tentatives de connexions échouées.
@@ -93,7 +93,7 @@ IDLE_TIME 10
 FAILED_LOGIN_ATTEMPTS 3
 PASSWORD_LOCK_TIME 20/1440
 PASSWORD_LIFE_TIME 30
-PASSWORD_GRACE_TIME 3
+PASSWORD_GRACE_TIME 3;
 
 
 
@@ -101,3 +101,8 @@ GRANT r_gestion TO JAD_gestionnaire;
 GRANT r_registrariat TO JAD_registrariat;
 GRANT r_api TO JAD_api;
 GRANT r_enseignant TO JAD_enseignant;
+
+ALTER USER JAD_gestionnaire PROFILE p_gestion;
+ALTER USER JAD_registrariat PROFILE p_usager;
+ALTER USER JAD_api PROFILE p_usager;
+ALTER USER JAD_enseignant PROFILE p_usager;
