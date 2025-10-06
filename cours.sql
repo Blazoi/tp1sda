4973beddd9d8252a2bc2b540ef32e639d9107324
Blazoi <jackjackdieu@gmail.com>
2025-10-06 17:29:02 -0400

    Fini les roles et profils

--- cours.sql
+++ cours.sql
@@ -1,32 +1,62 @@
+-- Rôles
+-- Toutes les permessions sur la base de donnée offertes au gestionnaire
+CREATE role r_gestion;
+GRANT DBA TO r_gestion;
 
+-- Toutes les permessions offertes sur les tables respectives pour le reste des utilisateurs
+CREATE ROLE r_registrariat;
+GRANT CREATE SESSION TO r_registrariat;
+GRANT ALL PRIVILEGES ON etudiants to r_registrariat;
+GRANT ALL PRIVILEGES ON cours to r_registrariat;
+
+CREATE ROLE r_api;
+GRANT CREATE SESSION TO r_api;
+GRANT ALL PRIVILEGES ON groupes to r_api;
+GRANT ALL PRIVILEGES ON semestres to r_api;
+
+CREATE ROLE r_enseignant;
+GRANT CREATE SESSION TO r_enseignant;
+GRANT ALL PRIVILEGES ON evaluations TO r_enseignant;
+
+-- Profils
+-- Limitations sont beaucoup moins grandes ce qui rend ce profil moins restrictif
+CREATE PROFILE p_gestion LIMIT
+IDLE_TIME 30
+FAILED_LOGIN_ATTEMPTS 5
+PASSWORD_LOCK_TIME 5/1440
+PASSWORD_LIFE_TIME 90
+PASSWORD_GRACE_TIME 7
+
+-- Limitations sont beaucoup plus strictes et standard avec des conséquences plus grandes
+-- de tentatives de connexions échouées.
+CREATE PROFILE p_usager LIMIT
+IDLE_TIME 10
+FAILED_LOGIN_ATTEMPTS 3
+PASSWORD_LOCK_TIME 20/1440
+PASSWORD_LIFE_TIME 30
+PASSWORD_GRACE_TIME 3
+
+
+-- Utilisateurs
 -- DBA principal et son collègue
 CREATE USER sys1 IDENTIFIED BY oracle;
 CREATE USER sys2 IDENTIFIED BY oracle;
--- Rôle de DBA parce qu'ils sont les administrateurs de la base de données.
-GRANT DBA TO sys1;
-GRANT DBA TO sys2;
+-- Rôle de SYSDBA parce qu'ils sont les administrateurs de la base de données.
+GRANT SYSDBA TO sys1;
+GRANT SYSDBA TO sys2;
 
 -- Gestionnaire
--- Permissions connect et resource parce qu'elles vont lui permettre de porter des modifications à la base de donnée sans pour autant égaler les DBAs.
 CREATE USER gestionnaire IDENTIFIED BY oracle;
-GRANT CONNECT, RESOURCE TO gestionnaire;
+GRANT r_gestion TO gestionnaire;
 
 -- Registrariat:
--- Permission connect parce qu'ils ne font que gérer élèves et cours.
 CREATE USER registrariat IDENTIFIED BY oracle
-GRANT CONNECT ON etudiant, cours TO registrariat;
+GRANT r_registrariat TO registrariat;
 
 -- API
--- Permission connect parce qu'ils ne font que gérer élèves et cours.
 CREATE USER api IDENTIFIED BY oracle
-GRANT CONNECT ON groupe, semestre TO api;
+GRANT r_api TO api;
 
 -- Enseignant
--- Permission connect parce qu'ils ne font que gérer élèves et cours.
 CREATE USER enseignant IDENTIFIED BY oracle
-GRANT CONNECT ON evaluations TO enseignant;
-
-
-
-CREATE PROFILE profil1 LIMIT
-;
+GRANT r_enseignant TO enseignant;
